faaab60ca56d0141e0b235350d32327c4ffdf3a7
Tim Huegdon <tim@nefariousdesigns.co.uk>
2010-08-25 21:08:46 +0100

    More transition stuff.

--- js/jquery.gallery.transitions.js
+++ js/jquery.gallery.transitions.js
@@ -90,4 +90,42 @@
             }
         });
     }
+    
+    $.fn.gallery.curtain = function(config) {
+        var leftPane    = config.old.css({
+                border:                 "0",
+                width:                  "50%",
+                backgroundPosition:     "0 0",
+                left:                   ((config.new.outerWidth() - config.new.width()) / 2) + 'px',
+                top:                    ((config.new.outerHeight() - config.new.height()) / 2) + 'px'
+            }),
+            rightPane   = config.old.clone().css({
+                border:                 "0",
+                width:                  "50%",
+                backgroundPosition:     -config.old.get(0).offsetWidth + 'px 0',
+                left:                   config.old.get(0).offsetWidth
+            });
+        
+        config.view.append(rightPane);
+        leftPane.animate({
+            left:       -leftPane.get(0).offsetWidth + 'px',
+            opacity:    0
+        }, {
+            duration:   config.duration,
+            queue:      false,
+            complete:   function(e) {
+                $(this).remove();
+            }
+        });
+        rightPane.animate({
+            left:       rightPane.position().left + rightPane.get(0).offsetWidth + 'px',
+            opacity:    0
+        }, {
+            duration:   config.duration,
+            queue:      false,
+            complete:   function(e) {
+                $(this).remove();
+            }
+        });
+    }
 })(jQuery);
